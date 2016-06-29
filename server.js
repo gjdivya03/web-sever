869d28d0921a73d2dbe2ccc1eb7f9bbd67c17f37
Divya <divyamounica@chargeback360.com>
2016-06-29 17:01:23 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require ('express');
+
+var app = express();
+var port =3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log('Request:' + new Date().toString + ' ' + req.method + '' + req.originalUrl);
+		next();
+	}
+};
+app.use(middleware.logger);
+// app.use(middleware.requireAuthentication);
+// app.get('/', function (req, res) {
+// 	res.send('hello EXPRESS!');
+// });
+app.get('/about',middleware.requireAuthentication, function (req, res) {
+	res.send('we are learning');
+});
+
+app.use(express.static(__dirname + '/public'));
+// console.log(__dirname);
+app.listen(port, function() {
+	console.log('express srever started on port' + port + '!');
+});
